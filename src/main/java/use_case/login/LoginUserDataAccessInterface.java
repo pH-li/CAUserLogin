e56b614587d673e7d0cb7a9c285b4a52a1535f69
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-10-28 00:17:02 -0400

    Finished task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user to the given username.
+     * @param name the name of the user
+     */
+    void setCurrentUser(String name);
 }
